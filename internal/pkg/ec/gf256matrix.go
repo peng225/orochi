8c04b28859bd733758a90567ea529f1cebef901d
Shinya Hayashi <shinyaUT0925@gmail.com>
2025-11-02 01:04:06 +0900

    add matrix type over GF(256)
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

---
+++ internal/pkg/ec/gf256matrix.go
@@ -0,0 +1,143 @@
+package ec
+
+import (
+	"errors"
+	"fmt"
+)
+
+type GF256Matrix struct {
+	numRows int
+	data    []gf256
+}
+
+func NewGF256Matrix(numRows, numCols int) *GF256Matrix {
+	if numRows <= 0 {
+		panic("numRows must be positive")
+	}
+	if numCols <= 0 {
+		panic("numCols must be positive")
+	}
+	return &GF256Matrix{
+		numRows: numRows,
+		data:    make([]gf256, numRows*numCols),
+	}
+}
+
+func NewGF256MatrixWithData(numRows int, data []gf256) *GF256Matrix {
+	if numRows <= 0 {
+		panic("numRows must be positive")
+	}
+	if len(data)%numRows != 0 {
+		panic("len(data) must be divisible by numRows")
+	}
+	return &GF256Matrix{
+		numRows: numRows,
+		data:    data,
+	}
+}
+
+func (mat *GF256Matrix) NumCols() int {
+	return len(mat.data) / mat.numRows
+}
+
+func (mat *GF256Matrix) Get(i, j int) gf256 {
+	return mat.data[i*mat.NumCols()+j]
+}
+
+func (mat *GF256Matrix) Set(i, j int, v gf256) {
+	mat.data[i*mat.NumCols()+j] = v
+}
+
+func (mat *GF256Matrix) swapRows(i, j int) error {
+	if i < 0 || j < 0 {
+		return fmt.Errorf("both i and j must be positive: i=%d, j=%d", i, j)
+	}
+	if i >= mat.numRows || j >= mat.numRows {
+		return fmt.Errorf("both i and j must be smaller than numRows: numROws=%d i=%d, j=%d",
+			mat.numRows, i, j)
+	}
+	for k := range len(mat.data) / mat.numRows {
+		mat.data[i*mat.numRows+k], mat.data[j*mat.numRows+k] = mat.data[j*mat.numRows+k], mat.data[i*mat.numRows+k]
+	}
+	return nil
+}
+
+func (mat *GF256Matrix) MulRight(m *GF256Matrix) (*GF256Matrix, error) {
+	if mat.NumCols() != m.numRows {
+		return nil, fmt.Errorf("matrix size mismatch")
+	}
+	result := NewGF256Matrix(mat.numRows, m.NumCols())
+	for i := range mat.numRows {
+		for j := range m.NumCols() {
+			var sum gf256
+			for k := range mat.NumCols() {
+				sum = sum.Add(mat.Get(i, k).Mul(m.Get(k, j)))
+			}
+			result.Set(i, j, sum)
+		}
+	}
+	return result, nil
+}
+
+func (mat *GF256Matrix) Inverse() (*GF256Matrix, error) {
+	if mat.numRows != len(mat.data)/mat.numRows {
+		return nil, errors.New("matrix must be square")
+	}
+
+	// Expansion matrix [A | I]
+	aug := NewGF256Matrix(mat.numRows, 2*mat.numRows)
+	for i := range mat.numRows {
+		for j := range mat.NumCols() {
+			aug.Set(i, j, mat.Get(i, j))
+		}
+		aug.Set(i, mat.NumCols()+i, 1)
+	}
+
+	// Gauss–Jordan elimination
+	for i := range aug.NumCols() / 2 {
+		// If the pivot is 0, exchange with another row.
+		if aug.Get(i, i) == 0 {
+			found := false
+			for k := i + 1; k < aug.numRows; k++ {
+				if aug.Get(k, i) != 0 {
+					err := aug.swapRows(i, k)
+					if err != nil {
+						return nil, err
+					}
+					found = true
+					break
+				}
+			}
+			if !found {
+				return nil, errors.New("matrix is singular")
+			}
+		}
+
+		// Normalize elements by the inverse of the pivot.
+		pivotInv := aug.Get(i, i).Inv()
+		for j := range aug.NumCols() {
+			aug.Set(i, j, aug.Get(i, j).Mul(pivotInv))
+		}
+
+		// For column i, delete values in other rows.
+		for k := range aug.numRows {
+			if k == i {
+				continue
+			}
+			factor := aug.Get(k, i)
+			if factor != 0 {
+				for j := range aug.NumCols() {
+					aug.Set(k, j, aug.Get(k, j).Sub(factor.Mul(aug.Get(i, j))))
+				}
+			}
+		}
+	}
+
+	inv := NewGF256Matrix(mat.numRows, mat.numRows)
+	for i := range inv.numRows {
+		for j := range inv.NumCols() {
+			inv.Set(i, j, aug.Get(i, aug.NumCols()/2+j))
+		}
+	}
+	return inv, nil
+}
---
+++ internal/pkg/ec/gf256matrix_test.go
@@ -0,0 +1,81 @@
+package ec
+
+import (
+	"slices"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGet(t *testing.T) {
+	mat := NewGF256MatrixWithData(2, []gf256{1, 2, 3, 4})
+	require.Equal(t, gf256(1), mat.Get(0, 0))
+	require.Equal(t, gf256(2), mat.Get(0, 1))
+	require.Equal(t, gf256(3), mat.Get(1, 0))
+	require.Equal(t, gf256(4), mat.Get(1, 1))
+}
+
+func TestSet(t *testing.T) {
+	mat := NewGF256Matrix(2, 3)
+	mat.Set(1, 1, 0x5)
+	require.Equal(t, gf256(0x5), mat.Get(1, 1))
+	mat.Set(1, 2, 0x6)
+	require.Equal(t, gf256(0x6), mat.Get(1, 2))
+}
+
+func TestInverse(t *testing.T) {
+	testCases := []struct {
+		name     string
+		m        *GF256Matrix
+		expected *GF256Matrix
+	}{
+		{
+			name:     "inverse of the unit matrix",
+			m:        NewGF256MatrixWithData(2, []gf256{1, 0, 0, 1}),
+			expected: NewGF256MatrixWithData(2, []gf256{1, 0, 0, 1}),
+		},
+		{
+			name:     "other",
+			m:        NewGF256MatrixWithData(2, []gf256{0x28, 0x3, 0xc8, 0x4}),
+			expected: NewGF256MatrixWithData(2, []gf256{0x81, 0x26, 0x5a, 0x7d}),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			inv, err := tc.m.Inverse()
+			require.NoError(t, err)
+			require.True(t, slices.Equal(tc.expected.data, inv.data))
+		})
+	}
+}
+
+func TestMulRight(t *testing.T) {
+	testCases := []struct {
+		name     string
+		m1       *GF256Matrix
+		m2       *GF256Matrix
+		expected *GF256Matrix
+	}{
+		{
+			name:     "I * I",
+			m1:       NewGF256MatrixWithData(2, []gf256{1, 0, 0, 1}),
+			m2:       NewGF256MatrixWithData(2, []gf256{1, 0, 0, 1}),
+			expected: NewGF256MatrixWithData(2, []gf256{1, 0, 0, 1}),
+		},
+		{
+			name:     "not square",
+			m1:       NewGF256MatrixWithData(2, []gf256{0x81, 0x2, 0x1, 0x14, 0x0, 0xc1}),
+			m2:       NewGF256MatrixWithData(3, []gf256{0x1, 0x20, 0x20, 0x3, 0xc2, 0x5}),
+			expected: NewGF256MatrixWithData(2, []gf256{0x3, 0x88, 0x7c, 0x5e}),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			mul, err := tc.m1.MulRight(tc.m2)
+			require.NoError(t, err)
+			require.True(t, slices.Equal(tc.expected.data, mul.data))
+		})
+	}
+}
