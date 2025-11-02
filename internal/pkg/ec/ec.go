fef38824c9ab2749b1a4dede094655e74fa4522c
Shinya Hayashi <shinyaUT0925@gmail.com>
2025-11-02 17:36:25 +0900

    add erasure coding
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

---
+++ internal/pkg/ec/ec.go
@@ -0,0 +1,220 @@
+package ec
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"maps"
+	"math"
+	"slices"
+)
+
+var (
+	ErrInvalidParameter    error = errors.New("invalid parameter")
+	ErrTooLargeData        error = errors.New("too large data")
+	ErrTooManyMissingCodes error = errors.New("too many missing codes")
+)
+
+const (
+	headerSizeInByte = 4
+)
+
+type Manager struct {
+	numData            int
+	numParity          int
+	minChunkSizeInByte int
+	maxDataSizeInByte  int64
+	generatorMatrix    *GF256Matrix
+}
+
+func NewManager(
+	numData, numParity, minChunkSizeInByte int,
+	maxDataSizeInByte int64,
+) *Manager {
+	if numData <= 0 {
+		panic("numData should be positive")
+	}
+	if numParity <= 0 {
+		panic("numParity should be positive")
+	}
+	if minChunkSizeInByte <= 0 {
+		panic("minChunkSizeInByte should be positive")
+	}
+	if maxDataSizeInByte <= 0 {
+		panic("maxDataSizeinByte should be positive")
+	}
+	// Calc parity blocks.
+	// Choose 1, a, a^2, ... a^(d+p-1) as evaluation points.
+	generatorMatrix := NewGF256Matrix(numData+numParity, numData)
+	for i := range numData {
+		for j := range numData {
+			if i == j {
+				generatorMatrix.Set(i, j, 1)
+			}
+		}
+	}
+	for i := range numParity {
+		val := gf256(1)
+		evalPoint := Gf256Generator().Pow(numData + i)
+		for j := range numData {
+			generatorMatrix.Set(numData+i, j, val)
+			val = val.Mul(evalPoint)
+		}
+	}
+
+	return &Manager{
+		numData:            numData,
+		numParity:          numParity,
+		minChunkSizeInByte: minChunkSizeInByte,
+		maxDataSizeInByte:  maxDataSizeInByte,
+		generatorMatrix:    generatorMatrix,
+	}
+}
+
+func (m *Manager) Encode(data []byte) ([][]byte, error) {
+	if len(data) > int(m.maxDataSizeInByte) {
+		return nil, ErrTooLargeData
+	}
+
+	result := make([][]byte, 0)
+	dataSize := uint32(len(data))
+	// Set data chunks.
+	if len(data)+headerSizeInByte*m.numData < m.numData*m.minChunkSizeInByte {
+		for len(data) != 0 {
+			chunkEndOffset := min(m.minChunkSizeInByte-headerSizeInByte, len(data))
+			buf := make([]byte, headerSizeInByte, headerSizeInByte+chunkEndOffset)
+			binary.LittleEndian.PutUint32(buf, dataSize)
+			buf = append(buf, data[0:chunkEndOffset]...)
+			result = append(result, buf)
+			data = data[chunkEndOffset:]
+		}
+		for range m.numData - len(result) {
+			buf := make([]byte, headerSizeInByte)
+			binary.LittleEndian.PutUint32(buf, dataSize)
+			result = append(result, buf)
+		}
+	} else {
+		chunkSize := int(
+			math.Ceil(
+				float64(len(data))/float64(m.numData),
+			),
+		) + headerSizeInByte
+		for len(data) != 0 {
+			chunkEndOffset := min(chunkSize-headerSizeInByte, len(data))
+			buf := make([]byte, headerSizeInByte, headerSizeInByte+chunkEndOffset)
+			binary.LittleEndian.PutUint32(buf, dataSize)
+			buf = append(buf, data[0:chunkEndOffset]...)
+			result = append(result, buf)
+			data = data[chunkEndOffset:]
+		}
+	}
+
+	// Set parity chunks.
+	for range m.numParity {
+		result = append(result, make([]byte, 0))
+	}
+	for offsetInData := range len(result[0]) {
+		for parityID := range m.numParity {
+			parityCode := gf256(0)
+			for dataID := range m.numData {
+				if len(result[dataID]) <= offsetInData {
+					continue
+				}
+				parityCode = gf256(parityCode).Add(
+					m.generatorMatrix.Get(m.numData+parityID, dataID).Mul(
+						gf256(result[dataID][offsetInData]),
+					),
+				)
+			}
+			result[m.numData+parityID] = append(result[m.numData+parityID], byte(parityCode))
+		}
+	}
+
+	return result, nil
+}
+
+func (m *Manager) Decode(codes [][]byte) ([]byte, error) {
+	if len(codes) != m.numData+m.numParity {
+		return nil, ErrInvalidParameter
+	}
+	remainingChunkIDs := make(map[int]struct{})
+	missingChunkIDs := make(map[int]struct{})
+	missingDataChunkExists := false
+	for i, v := range codes {
+		if len(v) == 0 {
+			missingChunkIDs[i] = struct{}{}
+			if i < m.numData {
+				missingDataChunkExists = true
+			}
+		} else {
+			remainingChunkIDs[i] = struct{}{}
+		}
+	}
+	if len(remainingChunkIDs) < m.numData {
+		return nil, ErrTooManyMissingCodes
+	}
+
+	chunkSize := 0
+	for _, c := range codes {
+		if chunkSize < len(c) {
+			chunkSize = len(c)
+		}
+	}
+	data := make([]byte, 0, (chunkSize-headerSizeInByte)*m.numData)
+	if !missingDataChunkExists {
+		for i := range m.numData {
+			data = append(data, codes[i][headerSizeInByte:]...)
+		}
+		return data, nil
+	}
+
+	remainingGenMatrix := NewGF256Matrix(m.numData, m.numData)
+	row := 0
+	for i := range len(codes) {
+		if _, ok := remainingChunkIDs[i]; !ok {
+			continue
+		}
+		for j := range m.numData {
+			remainingGenMatrix.Set(row, j, m.generatorMatrix.Get(i, j))
+		}
+		row += 1
+		if row == remainingGenMatrix.numRows {
+			break
+		}
+	}
+	inv, err := remainingGenMatrix.Inverse()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get the inverse of remaining generator matrix: %w", err)
+	}
+
+	for id := range missingChunkIDs {
+		codes[id] = make([]byte, 0, chunkSize)
+	}
+	sortedRemChunkIDs := slices.Sorted(maps.Keys(remainingChunkIDs))
+	for i := range chunkSize {
+		remainingSymbolsVec := NewGF256Matrix(m.numData, 1)
+		for j, rcID := range sortedRemChunkIDs[:m.numData] {
+			if i < len(codes[rcID]) {
+				remainingSymbolsVec.Set(j, 0, gf256(codes[rcID][i]))
+			}
+		}
+		decodedData, err := inv.MulRight(remainingSymbolsVec)
+		if err != nil {
+			return nil, fmt.Errorf("matrix multiplication failed: %w", err)
+		}
+		if decodedData.numRows != m.numData {
+			return nil, fmt.Errorf("unexpected decoded data vector size")
+		}
+		for id := range missingChunkIDs {
+			if id < m.numData {
+				codes[id] = append(codes[id], byte(decodedData.Get(id, 0)))
+			}
+		}
+	}
+
+	for i := range m.numData {
+		data = append(data, codes[i][headerSizeInByte:]...)
+	}
+	dataSize := binary.LittleEndian.Uint32(codes[0][:headerSizeInByte])
+	return data[:dataSize], nil
+}
---
+++ internal/pkg/ec/ec_test.go
@@ -0,0 +1,146 @@
+package ec
+
+import (
+	"slices"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestEncodeDecode_NoMissingChunks(t *testing.T) {
+	testCases := []struct {
+		name               string
+		numData            int
+		numParity          int
+		minChunkSizeInByte int
+		data               []byte
+	}{
+		{
+			name:               "(2D1P) data size is smaller than numData*minChunkSizeInByte",
+			numData:            2,
+			numParity:          1,
+			minChunkSizeInByte: 12,
+			data:               []byte("abcdefghij"),
+		},
+		{
+			name:               "(2D1P) data size is larger than numData*minChunkSizeInByte",
+			numData:            2,
+			numParity:          1,
+			minChunkSizeInByte: 12,
+			data:               []byte("abcdefghijklmnopqrstuv"),
+		},
+		{
+			name:               "(3D2P) data size is smaller than numData*minChunkSizeInByte",
+			numData:            3,
+			numParity:          2,
+			minChunkSizeInByte: 12,
+			data:               []byte("abcdefghij"),
+		},
+		{
+			name:               "(3D2P) data size is larger than numData*minChunkSizeInByte",
+			numData:            3,
+			numParity:          2,
+			minChunkSizeInByte: 12,
+			data:               []byte("abcdefghijklmnopqrstuvwxyz"),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			m := NewManager(tc.numData, tc.numParity,
+				tc.minChunkSizeInByte, 1000)
+			codes, err := m.Encode(tc.data)
+			require.NoError(t, err)
+			require.Len(t, codes, tc.numData+tc.numParity)
+
+			data, err := m.Decode(codes)
+			require.NoError(t, err)
+			require.True(t, slices.Equal(data, tc.data))
+		})
+	}
+}
+
+func TestEncodeDecode_RecoverFromMissingChunks(t *testing.T) {
+	testCases := []struct {
+		name               string
+		numData            int
+		numParity          int
+		minChunkSizeInByte int
+		data               []byte
+		missingChunkIDs    []int
+	}{
+		{
+			name:               "(2D1P) data size is smaller than numData*minChunkSizeInByte with first data chunk missing",
+			numData:            2,
+			numParity:          1,
+			minChunkSizeInByte: 12,
+			data:               []byte("abcdefghij"),
+			missingChunkIDs:    []int{0},
+		},
+		{
+			name:               "(2D1P) data size is smaller than numData*minChunkSizeInByte with second data chunk missing",
+			numData:            2,
+			numParity:          1,
+			minChunkSizeInByte: 12,
+			data:               []byte("abcdefghij"),
+			missingChunkIDs:    []int{1},
+		},
+		{
+			name:               "(2D1P) data size is smaller than numData*minChunkSizeInByte with parity chunk missing",
+			numData:            2,
+			numParity:          1,
+			minChunkSizeInByte: 12,
+			data:               []byte("abcdefghij"),
+			missingChunkIDs:    []int{1},
+		},
+		{
+			name:               "(2D1P) data size is larger than numData*minChunkSizeInByte with first data chunk missing",
+			numData:            2,
+			numParity:          1,
+			minChunkSizeInByte: 12,
+			data:               []byte("abcdefghijklmnopqrstuv"),
+			missingChunkIDs:    []int{0},
+		},
+		{
+			name:               "(3D2P) data size is smaller than numData*minChunkSizeInByte with second data chunk missing",
+			numData:            3,
+			numParity:          2,
+			minChunkSizeInByte: 12,
+			data:               []byte("abcdefghij"),
+			missingChunkIDs:    []int{1},
+		},
+		{
+			name:               "(3D2P) data size is larger than numData*minChunkSizeInByte with first and third data chunk missing",
+			numData:            3,
+			numParity:          2,
+			minChunkSizeInByte: 12,
+			data:               []byte("abcdefghijklmnopqrstuvwxyz"),
+			missingChunkIDs:    []int{0, 2},
+		},
+		{
+			name:               "(3D2P) data size is larger than numData*minChunkSizeInByte with data and parity chunk missing",
+			numData:            3,
+			numParity:          2,
+			minChunkSizeInByte: 12,
+			data:               []byte("abcdefghijklmnopqrstuvwxyz"),
+			missingChunkIDs:    []int{0, 4},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			m := NewManager(tc.numData, tc.numParity,
+				tc.minChunkSizeInByte, 1000)
+			codes, err := m.Encode(tc.data)
+			require.NoError(t, err)
+			require.Len(t, codes, tc.numData+tc.numParity)
+
+			for _, v := range tc.missingChunkIDs {
+				codes[v] = nil
+			}
+			data, err := m.Decode(codes)
+			require.NoError(t, err)
+			require.True(t, slices.Equal(data, tc.data))
+		})
+	}
+}
